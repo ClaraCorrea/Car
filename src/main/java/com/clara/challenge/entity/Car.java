44da9a6c44064f63c9fa3ab18d97d80de9abf484
ClaraCorrea <clara18055@live.com>
2023-07-13 13:26:16 -0300

    entity

---
+++ src/main/java/com/clara/challenge/entity/Car.java
@@ -0,0 +1,70 @@
+package com.clara.challenge.entity;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+
+@Entity
+@Table(name = "car")
+public class Car {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long idChasse;
+	
+	@NotBlank(message = "o campo é obrigatório!")
+	private String name, color, fabricationYear;
+	
+	@NotBlank(message = "o campo é obrigatório!")
+	private String brand;
+
+/*	public Car(String name, String color, String fabricationYear, String brand) {
+		this.name = name;
+		this.color = color;
+		this.fabricationYear = fabricationYear;
+		this.brand = brand;
+	}*/
+	
+	
+	public Long getIdChasse() {
+		return idChasse;
+	}
+
+	public void setIdChasse(Long idChasse) {
+		this.idChasse = idChasse;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public String getFabricationYear() {
+		return fabricationYear;
+	}
+
+	public void setFabricationYear(String fabricationYear) {
+		this.fabricationYear = fabricationYear;
+	}
+
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	
+	
+}
