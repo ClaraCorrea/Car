210a44513d5fda9b34b332804c32734e71c5346b
ClaraCorrea <clara18055@live.com>
2023-07-13 15:00:14 -0300

    carController

--- src/main/java/com/clara/challenge/controller/CarController.java
+++ src/main/java/com/clara/challenge/controller/CarController.java
@@ -3,6 +3,8 @@ package com.clara.challenge.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import com.clara.challenge.dto.CarDtoRequest;
+import com.clara.challenge.entity.Car;
 import com.clara.challenge.service.CarService;
 
 @RestController
@@ -17,14 +19,14 @@ public class CarController {
 		return carService.getString();
 	}
 	
-	@GetMapping("/get2")
-	public String get2() {
-		return carService.getString();
+	@PostMapping("/post")
+	public Car post(@RequestBody CarDtoRequest carDto) {
+		return carService.save(carDto);
 	}
 	
-	@GetMapping("/get3")
-	public String get3() {
-		return "valor do controller";
+	@GetMapping("/get2/{id}")
+	public Car get2(@PathVariable Long id) {
+		return carService.getById(id);
 	}
 
 }
