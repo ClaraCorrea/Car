841297e44c05facea1c95eb4a6b93ccc623dae87
ClaraCorrea <clara18055@live.com>
2023-07-13 14:59:04 -0300

    service / carService

--- src/main/java/com/clara/challenge/service/CarService.java
+++ src/main/java/com/clara/challenge/service/CarService.java
@@ -2,6 +2,9 @@ package com.clara.challenge.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.clara.challenge.dto.CarDtoRequest;
+import com.clara.challenge.entity.Car;
 import com.clara.challenge.repository.CarRepository;
 
 @Service
@@ -10,11 +13,19 @@ public class CarService {
 	@Autowired
 	CarRepository carRepository;
 
-	public String getString() {
-		return "Valor vindo do service!";
-	}
-	
-/*	public Car saveCar(Car car2) {
+	public Car save(CarDtoRequest carDtoRequest) {
+		Car car = new Car(
+				carDtoRequest.getName(),
+				carDtoRequest.getColor(),
+				carDtoRequest.getFabricationYear(),
+				carDtoRequest.getBrand()
+				);
+		carRepository.save(car);
 		return car;
-	}*/
-}
+	}
+
+
+	public Car getById(Long id) {
+		return carRepository.findById(id).orElseThrow();
+	}
+
