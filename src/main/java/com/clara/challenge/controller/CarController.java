cda0cc06068e52721e7f7ff2fb5db312c2168aab
ClaraCorrea <clara18055@live.com>
2023-07-13 17:11:42 -0300

    controller

--- src/main/java/com/clara/challenge/controller/CarController.java
+++ src/main/java/com/clara/challenge/controller/CarController.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import com.clara.challenge.dto.CarDtoRequest;
-import com.clara.challenge.entity.Car;
+import com.clara.challenge.dto.CarDtoResponse;
 import com.clara.challenge.service.CarService;
 
 @RestController
@@ -14,18 +14,14 @@ public class CarController {
 	@Autowired
 	CarService carService;
 	
-	@GetMapping("/get")
-	public String get() {
-		return carService.getString();
-	}
-	
 	@PostMapping("/post")
-	public Car post(@RequestBody CarDtoRequest carDto) {
-		return carService.save(carDto);
+	public String post(@RequestBody CarDtoRequest carDto) {
+		carService.save(carDto);
+		return "Carro salvo no banco de dados!";
 	}
 	
-	@GetMapping("/get2/{id}")
-	public Car get2(@PathVariable Long id) {
+	@GetMapping("/get/{id}")
+	public CarDtoResponse get2(@PathVariable Long id) {
 		return carService.getById(id);
 	}
 
