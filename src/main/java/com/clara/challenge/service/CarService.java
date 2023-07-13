6e24ccc697124693506465760f2d158c4060374d
ClaraCorrea <clara18055@live.com>
2023-07-13 17:14:27 -0300

    carService a

--- src/main/java/com/clara/challenge/service/CarService.java
+++ src/main/java/com/clara/challenge/service/CarService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.clara.challenge.dto.CarDtoRequest;
+import com.clara.challenge.dto.CarDtoResponse;
 import com.clara.challenge.entity.Car;
 import com.clara.challenge.repository.CarRepository;
 
@@ -13,7 +14,7 @@ public class CarService {
 	@Autowired
 	CarRepository carRepository;
 
-	public Car save(CarDtoRequest carDtoRequest) {
+	public void save(CarDtoRequest carDtoRequest) {
 		Car car = new Car(
 				carDtoRequest.getName(),
 				carDtoRequest.getColor(),
@@ -21,11 +22,11 @@ public class CarService {
 				carDtoRequest.getBrand()
 				);
 		carRepository.save(car);
-		return car;
 	}
 
 
-	public Car getById(Long id) {
-		return carRepository.findById(id).orElseThrow();
+	public CarDtoResponse getById(Long id) {
+		Car car =  carRepository.findById(id).orElseThrow();
+		return null;
 	}
-
+}
